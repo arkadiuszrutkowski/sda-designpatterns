41b6988cd9951ba3bffbd9e26fa54b0f39686383
Arkadiusz Rutkowski <arkadiusz.rutkowski90@gmail.com>
2019-01-26 12:39:59 +0100

    Singleton - exercise 1

---
+++ src/main/java/pl/sda/creational/exercises/User.java
@@ -0,0 +1,19 @@
+package pl.sda.creational.exercises;
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/pl/sda/creational/exercises/UserStoreMain.java
@@ -0,0 +1,8 @@
+package pl.sda.creational.exercises;
+
+public class UserStoreMain {
+
+    public static void main(String[] args) {
+
+    }
+}
